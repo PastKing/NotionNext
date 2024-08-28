247855cc84c634d38b7fb332a715a058372c3e09
PastKing <3132883944@qq.com>
2024-08-29 07:29:52 +0800

    1

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,22 +1,15 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
-{/* <html lang="en">
-<head>
-    <meta name="baidu-site-verification" content="codeva-KwSrUBpKl5" />
-    <meta name="baidu-site-verification" content="codeva-dHm5TTCkvq" />
+{/* Ai摘要自定义头部 */}
+<Script src="https://gitee.com/past-dust/code-repository/raw/master/PastKingGPT.js" strategy="lazyOnload" />
 
-    <script src="https://gitee.com/past-dust/code-repository/raw/master/PastKingGPT.js"></script>
-
-</head>
-<body>
-    <script data-pjax defer>
-        new ChucklePostAI({
-            // 文章内容所在的元素属性的选择器, 也是AI挂载的容器, AI将会挂载到该容器的最前面
-            el: '#post>#notion-article',
-            summary_directly: true,
-            rec_method: 'web',
-            // 若网站开启了 PJAX, 则开启
-            pjax: true,
-        })
-    </script>
-</body>
-</html> */}
+{/* Ai摘要 */}
+<Script id="chuckle-post-ai" strategy="lazyOnload">
+  {`
+    new ChucklePostAI({
+      el: '#post>#notion-article',
+      summary_directly: true,
+      rec_method: 'web',
+      pjax: true,
+    })
+  `}
+</Script>
