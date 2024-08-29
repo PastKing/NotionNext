3e9af0b4c8d2c91edaf449f11463d921f7558d3b
PastKing <3132883944@qq.com>
2024-08-29 20:57:36 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -40,7 +40,7 @@ class MyDocument extends Document {
           <script data-pjax defer>
             {`
               new ChucklePostAI({
-                el: '#article-wrapper > article > section > #notion-article',
+                el: '#post>#article-wrapper > article > section >#notion-article',
                 summary_directly: true,
                 rec_method: 'web',
                 key: '123456',
